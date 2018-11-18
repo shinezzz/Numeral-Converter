574670420dbfcf8ffeeb6b9ece4db19c6d098858
zhouzhao <1879805956@qq.com>
2018-11-18 18:03:00 +0800

    creat a blank window based on notepad.py

--- NumeralConverter.py
+++ NumeralConverter.py
@@ -1,21 +1,46 @@
-import sys
+from PyQt5.QtGui import *
 from PyQt5.QtWidgets import *
-from PyQt5.QtGui import QIcon
+from PyQt5.QtCore import *
+from PyQt5.QtPrintSupport import *
 
-class MainWindow(QWidget):
+import os
+import sys
 
-    def __init__(self):
-       super().__init__()
-       self.titie = "Numeral Converter"
-       self.initUI()
 
-    def initUI(self):
-        """设置窗体的标题"""
-        self.setWindowTitle(self.titie)
-        """通过调用show()函数来显示窗口"""
+class MainWindow(QMainWindow):
+
+    def __init__(self, *args, **kwargs):
+        super(MainWindow, self).__init__(*args, **kwargs)
+
+        layout = QVBoxLayout()
+        self.editor = QPlainTextEdit()  # Could also use a QTextEdit and set self.editor.setAcceptRichText(False)
+
+
+        # # Setup the QTextEdit editor configuration
+        # fixedfont = QFontDatabase.systemFont(QFontDatabase.FixedFont)
+        # fixedfont.setPointSize(24)
+        # self.editor.setFont(fixedfont)
+
+        # self.path holds the path of the currently open file.
+        # If none, we haven't got a file open yet (or creating new).
+        # self.path = None
+
+        layout.addWidget(self.editor)
+
+        # container = QWidget()
+        # container.setLayout(layout)
+        # self.setCentralWidget(container)
+        #
+        # self.status = QStatusBar()
+        # self.setStatusBar(self.status)
+
+
         self.show()
 
 if __name__ == '__main__':
-    app = QApplication([])
+
+    app = QApplication(sys.argv)
+    app.setApplicationName("NumeralConverter")
+
     window = MainWindow()
-    sys.exit(app.exec_())
+    app.exec_()
